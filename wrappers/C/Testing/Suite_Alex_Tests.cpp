2ea8f5d7ad12b6f5bc7dd784563143724bbf81ae
alexdarling <aldarling@hotmail.com>
2014-08-29 23:47:43 -0700

    Updated personal tests with CMakeLists.txt based on conversations with Andy

--- wrappers/C/Testing/Suite_Alex_Tests.cpp
+++ wrappers/C/Testing/Suite_Alex_Tests.cpp
@@ -34,8 +34,6 @@ extern string   gTestDataFolder;
 extern string   gCompiler;
 
 
-string getListOfReactionsText(const string& fName);
-
 SUITE(ALEX_TESTS)
 {
     TEST(RESET_MODELS)
@@ -43,11 +41,11 @@ SUITE(ALEX_TESTS)
         RRHandle mRR1                = createRRInstanceEx (gTempFolder.c_str(), gCompiler.c_str());
         RRHandle mRR2                = createRRInstanceEx (gTempFolder.c_str(), gCompiler.c_str());
         string TestModelFileName     = joinPath(gTestDataFolder, "Test_1.xml");
-        loadSBMLFromFileE (aRR1, TestModelFileName.c_str(), true);
-        loadSBMLFromFile E(mRR2, TestModelFileName.c_str(), true);
+        loadSBMLFromFileE (mRR1, TestModelFileName.c_str(), true);
+        loadSBMLFromFileE (mRR2, TestModelFileName.c_str(), true);
 
-        CHECK (simulateEx (aRR, 0, 10, 100));
-        reset (aRR)
+        CHECK (simulateEx (mRR1, 0, 10, 100));
+        reset (mRR1);
         CHECK (mRR1 == mRR2);
         
         CHECK (setGlobalParameterByIndex (mRR1, 0, 20));
@@ -66,33 +64,4 @@ SUITE(ALEX_TESTS)
     {
 
     }
-}
-
-string getListOfReactionsText(const string& fName)
-{
-        ifstream in(joinPath(gTestDataFolder, "Test_1.xml").c_str());
-        InputSource src(in);
-        DOMParser parser;
-        AutoPtr<Document> pDoc = parser.parse(&src);
-        TreeWalker it(pDoc, Poco::XML::NodeFilter::SHOW_ALL);
-
-        Node* pNode = it.nextNode();
-        string result;
-        while(pNode)
-        {
-            clog<<pNode->nodeName()<<endl;
-            string nodeID = "listOfReactions";
-            if(toUpper(pNode->nodeName()) == toUpper(nodeID))
-            {
-                DOMWriter aWriter;
-                stringstream xml;
-                aWriter.writeNode(xml, pNode);
-                result = xml.str();
-                break;
-            }
-            pNode = it.nextNode();
-        }
-
-        result.erase( std::remove_if( result.begin(), result.end(), ::isspace ), result.end() );
-        return result;
 }
